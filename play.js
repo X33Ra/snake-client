bddbea859edfe22f87873f15aab0f15f27fad7c7
MrChan <mrkcwayne@gmail.com>
2023-06-17 23:32:40 -0600

    removed comments

--- play.js
+++ play.js
@@ -4,16 +4,3 @@ const { setupInput, handleUserInput } = require("./input");
 console.log("Connecting ...");
 const connection = connect();
 setupInput(connection, handleUserInput);
-
-
-// Ideas, SHOO!
-// } else if (key === 'l') {
-//   console.log('Sending message: LOL');
-//   connection.write('Say: LOL'); // Send a canned message to the server
-// } else if (key === 'h') {
-//   console.log('Sending message: Hello');
-//   connection.write('Say: Hello'); // Send a canned message to the server
-// } else if (key === 'x') {
-//   console.log('Sending message: LMAO U SUCK');
-//   connection.write('Say: You lose'); // Send a canned message to the server
-//   console.log('You lose'); // Display "you lose" message locally
