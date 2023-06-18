18860368a41c9ffbae27f8e04790aeb80e048944
MrChan <mrkcwayne@gmail.com>
2023-06-18 02:42:45 -0600

    added a callback function that will handle the special messages received from the server

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require("net");
 const { IP, PORT } = require("./constants");
 
 // Establishes a connection with the game server
-const connect = function() {
+const connect = function(messageCallback) {
   const conn = net.createConnection({
     host: IP,
     port: PORT
@@ -16,17 +16,17 @@ const connect = function() {
     conn.write('Name: SNK');
   });
 
-  // Handle server messages
+  
   conn.on('data', (data) => {
-    console.log('Server Says:', data);
-    
-    // Check if the client has died
+    console.log('Server Says: ', data);
     if (data === 'You died') {
-      console.log('You lose');
+      console.log('You Lose');
       conn.end();
+    } else {
+      messageCallback(data);
     }
   });
-
+  
   return conn;
 };
 
