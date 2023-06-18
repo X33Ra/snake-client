ff280dbb025fc7c8dd20e3e688fcb7a2ecb1b1e6
MrChan <mrkcwayne@gmail.com>
2023-06-17 23:34:54 -0600

    added conn.on('data', (data) => { console.log('Server Says' , data) }

--- client.js
+++ client.js
@@ -3,7 +3,6 @@ const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function() {
-  
   const conn = net.createConnection({
     host: IP,
     port: PORT
@@ -11,13 +10,24 @@ const connect = function() {
 
   conn.setEncoding("utf-8");
 
-  // Notifys the user someone connected
+  // Notifies the user someone connected
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: SNK');
   });
 
+  // Handle server messages
+  conn.on('data', (data) => {
+    console.log('Server Says:', data);
+    
+    // Check if the client has died
+    if (data === 'You died') {
+      console.log('You lose');
+      conn.end();
+    }
+  });
+
   return conn;
 };
 
-module.exports = {connect};
+module.exports = { connect };
